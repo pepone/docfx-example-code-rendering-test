20b4e9fa883166025bf2b04d9ba73cf6cc3431b7
Jose <jose@zeroc.com>
2023-05-04 11:30:01 +0200

    Docfx example code rendering test

---
+++ docfx_project/examples/Example01.cs
@@ -0,0 +1,13 @@
+// A simple example of using MyClass
+
+var c = new MyClass();
+
+c.Foo(() =>
+    {
+        // Just showing
+        if (bar)
+        {
+            // Increase counter
+            i++;
+        }
+    });
---
+++ src/MyClass.cs
@@ -0,0 +1,14 @@
+namespace MyApp;
+
+/// <summary>A nice class</summary>
+public class MyClass
+{
+    /// <summary>A nice operation.</summary>
+    /// <example>
+    /// <para>A simple example of using <c>MyOperation</c></para>
+    /// <code source="../docfx_project/examples/Example01.cs" lang="csharp" />
+    /// </example>
+    public void MyOperation()
+    {
+    }
+}
